71de770b57886a7e8313ebc0b0c29034adb41446
chatscrypt <54454487+chatscrypt@users.noreply.github.com>
2024-06-22 10:53:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,16 +27,17 @@ async function prepareFile(url) {
           stream: fs.createReadStream(filePath)};
 }
 
-function easyRequest(url, options) {
+const keepAliveAgent = new https.Agent({ keepAlive: true, maxSockets:1, keepAliveMses:10000 });
+function easyRequest(url, options, mainRes) {
     return new Promise((resolve, reject) => {
-        options.agent = keepAliveAgent;
+    //    options.agent = keepAliveAgent;
         const req = https.request(url, options, res => {
             const chunks = [];
             res.on('data', function(data) { chunks.push(data); });
             res.on('end', function() {
                 let resBody = Buffer.concat(chunks);
                 if (res.headers['content-type'] == 'application/json')
-                  resBody = JSON.parse(resBody);
+                  mainRes.setHeader('Content-Type', 'application/json');  
                 resolve(resBody);
             })
         });
@@ -54,10 +55,30 @@ function easyParse(req) {
     });
 }
 
-const keepAliveAgent = new https.Agent({ keepAlive: true, maxSockets:1, keepAliveMses:10000 });
+let rawProxyKey = process.env.PROXYKEY;
+var crypto = require('crypto').webcrypto;
 
-http.createServer(async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');         
+var proxyKey;
+importKey(rawProxyKey).then(function(key) {proxyKey = key;});
+
+async function generateKey() { console.log((await crypto.subtle.exportKey("jwk", await crypto.subtle.generateKey({name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"]))).k); }
+function importKey(raw) { return crypto.subtle.importKey("jwk", {alg:"A256GCM", ext:true, k:raw, key_ops:['encrypt', 'decrypt'], kty:"oct"}, "AES-GCM", true, ["encrypt", "decrypt"]); }
+
+async function encrypt(key, payload) {
+	let iv = crypto.getRandomValues(new Uint8Array(12));
+	return {cipher:_arrayBufferToBase64(await crypto.subtle.encrypt({name: "AES-GCM", iv}, key, new TextEncoder().encode(JSON.stringify(payload)))), 
+			iv:_arrayBufferToBase64(iv)};	
+}
+
+async function decrypt(key, cipherPackage) {
+	return JSON.parse(new TextDecoder().decode(await crypto.subtle.decrypt({name: "AES-GCM", iv:_base64ToArrayBuffer(cipherPackage.iv)}, key, _base64ToArrayBuffer(cipherPackage.cipher))));
+}
+
+function _arrayBufferToBase64(buffer) {	return Buffer.from(buffer).toString('base64'); }	
+function _base64ToArrayBuffer(base64) {	return Buffer.from(base64, 'base64'); }
+
+
+http.createServer(async (req, res) => {       
   res.setHeader('Access-Control-Allow-Origin', '*');      
   res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || '*');
   if (req.method == "OPTIONS") res.end();
@@ -68,15 +89,21 @@ http.createServer(async (req, res) => {
       res.writeHead(statusCode, {"Content-Type": MIME_TYPES[file.ext] || MIME_TYPES.default});
       file.stream.pipe(res);
       console.log(`${req.method} ${req.url} ${statusCode}`);
+    } else if (req.url == "/aes") {
+      let aesBody = await easyParse(req);
+      let {url, parameters} = await decrypt(proxyKey, JSON.parse(aesBody));
+      easyRequest(url, parameters || {method:"GET"}, res).then(function(data) {
+        res.write(data);
+        res.end();
+      });
     } else {
     //  console.log(req.url.substring(1), req.method);
       let body = await easyParse(req);
       let headers = req.headers.string && JSON.parse(req.headers.string);
       if (body && !(headers && headers['content-type']))
         headers['content-type'] = "application/x-www-form-urlencoded";
-      easyRequest(req.url.substring(1), {headers, method:req.method, body}).then(function(data) {
-        try { res.write(data); }
-        catch { res.write(JSON.stringify(data)); }
+      easyRequest(req.url.substring(1), {headers, method:req.method, body}, res).then(function(data) {
+        res.write(data);
         res.end();
       });
     }
